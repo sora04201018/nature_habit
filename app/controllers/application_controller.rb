09a2d23e649db0e9ca4a8b06186c0223fbc01f5a
荒河天羅 <arakawatenra@arakawatenranoMacBook-Air.local>
2025-11-10 19:43:37 +0900

    ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,16 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [ :name ])
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name ])
   end
+
+  # ログアウト後の遷移先を指定
+  def after_sign_out_path_for(resource_or_scope)
+    root_path # 今後ダッシュボードに遷移予定
+  end
+
+  # ログイン後の遷移先を指定
+  def after_sign_in_path_for(resource_or_scope)
+    root_path # 今後ダッシュボードに遷移予定
+  end
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
