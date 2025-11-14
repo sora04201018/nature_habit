7ce810eee016637d52b06daf36ede9eb82d12879
荒河天羅 <arakawatenra@arakawatenranoMacBook-Air.local>
2025-11-14 12:11:27 +0900

    dashboard_test01

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,6 +2,9 @@ require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    user = users(:one)
+    sign_in user
+
     get dashboard_index_url
     assert_response :success
   end
