261c8ba60f7c0e28b34c89f709e8479ab8975462
荒河天羅 <arakawatenra@arakawatenranoMacBook-Air.local>
2025-11-23 17:56:32 +0900

    lintチェック修正01

--- app/models/habit_check.rb
+++ app/models/habit_check.rb
@@ -3,5 +3,5 @@ class HabitCheck < ApplicationRecord
   belongs_to :habit
 
   validates :checked_on, presence: true
-  validates :habit_id, uniqueness: { scope: [:user_id, :checked_on] } # マイグレーション同様の制約。scopeでカラムの組み合わせ指定。
+  validates :habit_id, uniqueness: { scope: [ :user_id, :checked_on ] } # マイグレーション同様の制約。scopeでカラムの組み合わせ指定。
 end
--- db/migrate/20251120013249_create_habit_checks.rb
+++ db/migrate/20251120013249_create_habit_checks.rb
@@ -8,6 +8,6 @@ class CreateHabitChecks < ActiveRecord::Migration[7.2]
       t.timestamps
     end
     # 同じ習慣を同じ日に1回だけしか登録できないようにする。３つセットでユニーク制約。
-    add_index :habit_checks, [:user_id, :habit_id, :checked_on], unique: true, name: "index_habit_checks_on_user_habit_date"
+    add_index :habit_checks, [ :user_id, :habit_id, :checked_on ], unique: true, name: "index_habit_checks_on_user_habit_date"
   end
 end
