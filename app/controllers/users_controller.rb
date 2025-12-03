7c8fbba6a4bf285cabf2624bddbfb77e405d6447
荒河天羅 <arakawatenra@arakawatenranoMacBook-Air.local>
2025-12-03 12:50:57 +0900

    カレンダー専用ページでカレンダーを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,5 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user # ユーザー情報取得
-    @month = params[:month] ? Date.parse(params[:month]) : Date.current
-    @habit_checks = current_user.habit_checks.includes(:habit).where(checked_on: @month.beginning_of_month..@month.end_of_month) # 今ログインしているユーザーのチェック状況・習慣情報(タイトルなど)を取得 カレンダー表示のため
   end
 end
