9a4a0386eb6380d91052fea211ddc5edeb3865d1
荒河天羅 <arakawatenra@arakawatenranoMacBook-Air.local>
2025-12-09 13:36:35 +0900

    バッジのレイアウト/未解放バッジ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user # ユーザー情報取得
+
+    @total_habit_checks_count = @user.total_habit_checks_count
+
+    # ユーザー未獲得のバッジをマイページに表示
+    @owned_badges = @user.badges
+    @unowned_badges = Badge.where.not(id: @owned_badges.pluck(:id)) # where.notで条件に合わないものを返す。 pluck(:id)でidの配列で返す。
   end
 end
