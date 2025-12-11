216dacb1e99b49198d5b2ef0766c653a647b2fc0
荒河天羅 <arakawatenra@arakawatenranoMacBook-Air.local>
2025-12-11 10:12:05 +0900

    test修正01

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,23 +1,38 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+    sign_in @user
+
+    @post = posts(:one)
+  end
+
   test "should get index" do
-    get posts_index_url
+    get posts_path
     assert_response :success
   end
 
   test "should get show" do
-    get posts_show_url
+    get post_path(@post)
     assert_response :success
   end
 
   test "should get new" do
-    get posts_new_url
+    get new_post_path
     assert_response :success
   end
 
-  test "should get create" do
-    get posts_create_url
-    assert_response :success
+  test "should create post" do
+    assert_difference("Post.count", 1) do
+      post posts_path, params: {
+        post: {
+          title: "Test Title",
+          body: "Test Body"
+        }
+      }
+    end
+
+    assert_redirected_to post_path(Post.last)
   end
 end
