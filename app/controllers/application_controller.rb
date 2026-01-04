33da9acff383511743e97da4af0cb1def8dcacd5
荒河天羅 <arakawatenra@arakawatenranoMacBook-Air.local>
2026-01-04 20:40:03 +0900

    LINE認証機能の修正02

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token,
+    if: -> { request.path.start_with?("/users/auth/") }
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :user_signed_in_root
 
